938e796d80cc81951eb124fe0d7390b7d5897f9c
Rhys Hansen <rhyshonline@gmail.com>
2017-07-10 18:25:03 -0500

    Update init file with imports and better formatting

--- processdog/__init__.py
+++ processdog/__init__.py
@@ -1,5 +1,13 @@
+#!/usr/bin/env python3
+#coding=UTF-8
+from .subprocess_manager import ManagerThread
+from .subprocess_manager import WorkerThread
+
+major_version = 0
+minor_version = 0
+build_version = 1
+
 __author__ = 'Rhys Hansen'
 __copyright__ = "Copyright 2017, Rhys Hansen"
 __license__ = "MIT"
-__date__ = "2017-1-30"
-__version__ = '0.0.1'
+__version__ = '{0}.{1}.{2}'.format(major_version, minor_version, build_version)
